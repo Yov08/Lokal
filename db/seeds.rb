4a4358a31cd2d699479b060b586ef5f1c5cdb5d4
ashlay silvio <asilvio@morganphilips.com>
2023-02-11 18:55:26 +0400

    seeds for events , users and artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,102 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying events"
+Event.destroy_all
+puts "Creating events"
+Event.create!(
+  name: "festival creole",
+  date: "24.09.2023",
+  venue: "green park",
+  description: "an event for showing our culture",
+  price_normal: 500,
+  price_vip: 700,
+  address: "port louis",
+  capacity: 1000,
+  image_url: "http//portlouis.mu"
+)
+Event.create!(
+  name: "sakifo",
+  date: "24.10.2023",
+  venue: "red park",
+  description: "an unique event",
+  price_normal: 400,
+  price_vip: 1000,
+  address: "le morne",
+  capacity: 2000,
+  image_url: "http//mpta.mu"
+)
+Event.create!(
+  name: "embas pied",
+  date: "24.09.2023",
+  venue: "green park",
+  description: "an event for showing our culture",
+  price_normal: 500,
+  price_vip: 1000,
+  address: "le morne",
+  capacity: 2000,
+  image_url: "http//mpta.mu"
+)
+puts "Finished!"
+
+
+puts "Destroying artists"
+Artist.destroy_all
+puts "Creating artists"
+Artist.create!(
+  name: "kaya",
+  surname: "popise",
+  email: "kayaman@gmail.com",
+  category: "reggea",
+  hourly_rate: "200",
+  photo: "http//www.mra.com"
+)
+Artist.create!(
+  name: "annega",
+  surname: "annaise",
+  email: "annega@gmail.com",
+  category: "pop",
+  hourly_rate: "1000",
+  photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwearefreedom.studio%2Fcasestudy-annega%2F&psig=AOvVaw3Oj9k-cTSlpe2Y0qqhErVZ&ust=1676211177015000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOCNiq_Tjf0CFQAAAAAdAAAAABAE"
+)
+Artist.create!(
+  name: "hans",
+  surname: "nayna",
+  email: "kayaman@gmail.com",
+  category: "reggea",
+  hourly_rate: "200",
+  photo: "https://otayo.com/en/artists/hans-nayna/"
+)
+puts "Finished!"
+
+
+puts "Destroying users"
+User.destroy_all
+puts "Creating users"
+User.create!(
+  first_name: "damien",
+  surname: "popise",
+  email: "kayaman@gmail.com",
+  category: "reggea",
+  hourly_rate: "200",
+  photo: "http//www.mra.com"
+
+
+# require "faker"
+# puts "Creating 3 fake events"
+# 3.times do
+#   event =Event.new(
+#     name: Faker::Name.name,
+#     date: Faker::Date.between(from: '2023-09-23', to: '2024-09-24'),
+#     venue: Faker::Address.city,
+#     description: Faker::Lorem.unique.exclude,
+#     price_normal: Faker::Number.binary,
+#     price_vip: Faker::Number.binary,
+#     address: Faker::Address.secondary_address,
+#     capacity: Faker::Number.binary,
+#     image_url: Faker::Internet.domain_name(subdomain: true, domain: "example")
+#   )
+#   event.save!
+# end
+# puts "finished!"
