b865fb5c6a7d8b0ac88b5040a94dd45ff3fee905
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-02-11 15:34:20 +0400

    added event_artist model & controller

---
+++ app/controllers/event_artists_controller.rb
@@ -0,0 +1,2 @@
+class EventArtistsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_110719) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_112437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "event_artists", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
