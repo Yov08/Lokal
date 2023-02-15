e218a566262f2415f5deed4ccb0bd7f12e5ec5da
Yovilen Ramasamy <yovilen8@gmail.com>
2023-02-15 22:39:23 +0400

    user-controller-updates 1502

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save!
+    redirect_to users_path(@user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, status: :see_other
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :image_url, :category, :biography, :hourly_rate)
+  end
+
 end
