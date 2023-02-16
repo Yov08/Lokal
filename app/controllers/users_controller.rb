131d70ca08a4c0da48c06105331589f388ab1c15
Yovilen Ramasamy <yovilen8@gmail.com>
2023-02-16 19:08:34 +0400

    changes brought to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
-
-
   def show
     @user = User.find(params[:id])
   end
@@ -15,24 +10,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save!
-    redirect_to users_path(@user)
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to users_path, status: :see_other
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
