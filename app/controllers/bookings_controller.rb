9db1385a60785f176ec250130015873f695e3e22
ashlay silvio <asilvio@morganphilips.com>
2023-02-18 16:21:54 +0400

    adding detail to booking controller, model and creating booking file in view, to chekc if work s not user models not yet completed , update and delete bookings not done as not on figma

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,55 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = current_user.bookings
+    @events = Event.all
+    # @artists = Artist.all
+  end
+
+  def bookings
+    @bookingsall = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.price = @events.price + @artists.price
+    @booking.user_id = current_user.id
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking[:id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:pax, :booking_type, :total_price, :event_id)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,6 @@
 class Booking < ApplicationRecord
+  belongs_to :user, foreign_key: "user_id", class_name: "User"
+  has_many :events
+  validates :events_id, presence: true
+  validates :user_id, presence: true
 end
