822e70ff1383491aa2c73725345dc5d715b641c7
ashlay silvio <asilvio@morganphilips.com>
2023-03-02 21:44:02 +0400

    seeding working and link working

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,57 +48,36 @@ puts "Destroying artists"
 Artist.destroy_all
 puts "Creating artists"
 Artist.create!(
-  # first_name: "Jean Clario",
-  # last_name: "Gateaux",
-  # user_type: ,
+  name: "Jean Clario Gateaux",
+  user_id: 3,
   category: "Reggae",
   biography: "Blakkayo lives at Goodlands, in the north of Mauritius. He was born in 1978. In the 1990s, he performed as a dancer which led him to love music, specifically reggae music. While listening to Kaya’s seggae, a fusion of Mauritian sega and reggae, made him realise that he can use music to share his belief and help people see who and what are the reals. He even wrote his own lyrics which led to freestyle music.",
   rating: 4.8,
   hourly_rate: 300,
   media_link: "https://otayo.com/en/artists/blakkayo/",
   image_url: "https://scontent.fmru3-1.fna.fbcdn.net/v/t1.6435-9/162465982_294918178661000_4541438307956873517_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=e7RNbcwFaXQAX_0DCxs&_nc_ht=scontent.fmru3-1.fna&oh=00_AfCV9l-1fW62SXuLAGI29Otfclv5PeCTEHh_X5q6UzICLQ&oe=64259923",
-  social_media_links:"https://www.facebook.com/orizinalblakkayo/"
+  social_media_links: "https://www.facebook.com/orizinalblakkayo/"
 )
 Artist.create!(
-  # first_name: "Anne Gaëlle",
-  # last_name: "Bourquin",
-  # user_type: ,
+  name: "Anne Gaëlle Bourquin",
+  user_id: 3,
   category: "pop",
   biography: "Anne Gaëlle Bourquin, most commonly known as Anne Ga amongst the Mauritians. She started out on the stage of Timambo at the age of six. Her parents enrolled her in the conservatory where she learned classical piano and also joined the children’s choir.",
   rating: 5.8,
   hourly_rate: 1000,
   media_link: "https://annega.mu/",
   image_url: "https://otayo.com/wp-content/uploads/2022/07/anne-ga-4.jpg",
-  social_media_links:"https://www.facebook.com/Annegaofficial/"
+  social_media_links: "https://www.facebook.com/Annegaofficial/"
 )
 Artist.create!(
-  # first_name: "Hans",
-  # last_name: "Nayna",
-  # user_type: ,
+  name: "Hans Nayna",
+  user_id: 3,
   category: "Music for the Soul",
   biography: "Hans Nayna is a Mauritian singer, songwriter and a guitarist inspired by many international artists such as Sia, Coldplay, Adele. He writes songs that are filled with enotions. His devotion for music and writing came to him after his high school, where he grabbed a pen and started composing a song and added music to it using a guitar. He has recently released the song 'Music for the Soul'.",
   rating: 5.0,
   hourly_rate: 600,
   media_link: "https://ee.linkedin.com/in/hansnayna",
-  image_url: "https://scontent.fmru3-1.fna.fbcdn.net/v/t1.6435-9/162465982_294918178661000_4541438307956873517_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=e7RNbcwFaXQAX_0DCxs&_nc_ht=scontent.fmru3-1.fna&oh=00_AfCV9l-1fW62SXuLAGI29Otfclv5PeCTEHh_X5q6UzICLQ&oe=64259923",
-  social_media_links:"https://scontent.fmru3-1.fna.fbcdn.net/v/t1.6435-9/115824069_2777886295769681_7249292576166010466_n.jpg?stp=cp0_dst-jpg_e15_q65_s403x403&_nc_cat=107&ccb=1-7&_nc_sid=7aed08&_nc_ohc=5z9b7xiutr8AX_jPulB&_nc_ht=scontent.fmru3-1.fna&oh=00_AfCx4I1yIkToFtWGXuv--7OR--4SYaF8xka-BiknkGKZ8w&oe=6425AEE0"
+  image_url: "https://otayo.com/wp-content/uploads/2022/07/hans-nayna-1.jpg",
+  social_media_links: "https://scontent.fmru3-1.fna.fbcdn.net/v/t1.6435-9/115824069_2777886295769681_7249292576166010466_n.jpg?stp=cp0_dst-jpg_e15_q65_s403x403&_nc_cat=107&ccb=1-7&_nc_sid=7aed08&_nc_ohc=5z9b7xiutr8AX_jPulB&_nc_ht=scontent.fmru3-1.fna&oh=00_AfCx4I1yIkToFtWGXuv--7OR--4SYaF8xka-BiknkGKZ8w&oe=6425AEE0"
 )
 puts "Finished!"
-
-puts "Destroying artists"
-Artist.destroy_all
-puts "Creating artists"
-Artist.create!(
-  name: "Blakkayo",
-  category: "Mizik Lokal",
-  biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  rating: "4.8",
-  hourly_rate: "30000"
-)
-Artist.create!(
-  name: "Annega",
-  category: "Chilled",
-  biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  rating: "4.2",
-  hourly_rate: "15000"
-)
