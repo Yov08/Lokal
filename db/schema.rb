06703c70343800dadc1beeb69d899777973f4e73
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-03-02 13:44:55 +0400

    new migration: pax_vip column added to Bookings object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_142119) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_094242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142119) do
   end
 
   create_table "artists", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "category"
     t.string "biography"
     t.integer "rating"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142119) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pax_vip"
   end
 
   create_table "event_artists", force: :cascade do |t|
