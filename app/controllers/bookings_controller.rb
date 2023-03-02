944b74dd90a53ceb66bd6e74c62eb3d4ae6a28e8
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-03-02 14:00:17 +0400

    added feature for user to input number of tickets desired with JS for updated total price

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -43,6 +43,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:event_id, :user_id, :pax, :booking_type, :total_price)
+    params.require(:booking).permit(:event_id, :user_id, :pax, :pax_vip, :booking_type, :total_price)
   end
 end
