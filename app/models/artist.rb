7fce0000827372eaf85615bfdfab56320e484be7
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-03-04 12:06:52 +0400

    added artist/event

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,8 @@
 class Artist < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
+  has_many :event_artists
+  has_many :events, through: :event_artists
 
   validates :name, presence: true
 end
--- app/models/event_artist.rb
+++ app/models/event_artist.rb
@@ -1,2 +1,5 @@
 class EventArtist < ApplicationRecord
+  belongs_to :artist
+  belongs_to :event
+  has_many :artists
 end
