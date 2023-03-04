be073d89b0a067441cd9e7ece0b2913b5f31fba3
Yovilen Ramasamy <yovilen8@gmail.com>
2023-03-04 15:10:46 +0400

    merge

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+  against: [ :name, :description ],
+  associated_against: {
+    event: [ :name, :description ]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
+
 end
