c2767a8877cfec918feb4b96a3a87228a9342d7d
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-03-09 17:17:56 +0400

    events show page update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,21 @@
 class LikesController < ApplicationController
   before_action :set_event
-  
+
   def create
     @like = current_user.likes.new(event: @event)
     if @like.save
-      redirect_to @event, notice: 'Event was liked.'
+      render json: { liked: true }
     else
-      redirect_to @event, alert: 'Unable to like event.'
+      render json: { liked: false }
     end
   end
 
   def destroy
     @like = current_user.likes.find_by(event: @event)
     if @like.destroy
-      redirect_to @event, notice: 'Event was unliked.'
+      render json: { liked: false }
     else
-      redirect_to @event, alert: 'Unable to unlike event.'
+      render json: { liked: true }
     end
   end
 
@@ -24,5 +24,4 @@ class LikesController < ApplicationController
   def set_event
     @event = Event.find(params[:event_id])
   end
-
 end
